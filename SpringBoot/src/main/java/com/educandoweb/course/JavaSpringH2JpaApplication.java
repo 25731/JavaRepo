9e57363f59114ac874974371ca9fd313d0adaab1
user 25731 <witoldjan153@gmail.com>
2023-03-11 23:42:43 +0100

    com

---
+++ SpringBoot/src/main/java/com/educandoweb/course/JavaSpringH2JpaApplication.java
@@ -0,0 +1,13 @@
+package com.educandoweb.course;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaSpringH2JpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaSpringH2JpaApplication.class, args);
+	}
+
+}
